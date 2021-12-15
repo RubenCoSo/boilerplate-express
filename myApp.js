dfdbd3b4aed1cb61affd170dd5486d37f0360ecf
Ruben Corral <rubencorralsola@gmail.com>
2021-12-15 18:21:40 +0100

    dotenv use

--- myApp.js
+++ myApp.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
+
+const messageStyle = process.env.MESSAGE_STYLE
+
+
+app.get('/',(req,res)=>{
+    absolutePath = __dirname + "/views/index.html"
+    res.sendFile(absolutePath)
+  })
+  
+  app.use('/public', express.static(__dirname+"/public"))
+  
+  app.get('/json',(req,res)=>{
+    if(messageStyle === "uppercase"){
+      return res.json({"message": "HELLO JSON"})
+    }
+    return res.json({"message": "Hello json"})
+  })  
 
 
 
